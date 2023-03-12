76741358650f2e5e11c096fb0c938d25ff44a309
gwendal3555 <123754778+gwendal3555@users.noreply.github.com>
2023-03-12 10:09:10 +0100

    Add files via upload

---
+++ Race_domicile_traitement.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+# Charger le fichier CSV avec Pandas
+DL = pd.read_csv(r"C:\Users\33782\Desktop\M2 MSI\S2\Certification\Python\NYC_Dog_Licensing_Dataset.csv", sep=";")
+
+# Créer un tableau croisé de deux colonnes
+tableau_croise = pd.crosstab(DL["ZipCode"], DL["BreedName"])
+
+# Ajouter le comptage de la valeur "Shih Tzu" à la colonne BreedName
+tableau_croise["Shih Tzu"] = DL[DL["BreedName"] == "Shih Tzu"].groupby("ZipCode")["BreedName"].count()
+
+# Ajouter le comptage de la valeur "Shih Tzu" à la colonne BreedName
+tableau_croise["Yorkshire Terrier"] = DL[DL["BreedName"] == "Yorkshire Terrier"].groupby("ZipCode")["BreedName"].count()
+
+
+# Ajouter le comptage de la valeur "Chihuahua" à la colonne BreedName
+tableau_croise["Chihuahua"] = DL[DL["BreedName"] == "Chihuahua"].groupby("ZipCode")["BreedName"].count()
+
+# Ajouter le comptage de la valeur "Maltese" à la colonne BreedName
+tableau_croise["Maltese"] = DL[DL["BreedName"] == "Maltese"].groupby("ZipCode")["BreedName"].count()
+
+# Ajouter le comptage de la valeur "Labrador Retriever" à la colonne BreedName
+tableau_croise["Labrador Retriever"] = DL[DL["BreedName"] == "Labrador Retriever"].groupby("ZipCode")["BreedName"].count()
+
+# Définir les pondérations pour chaque race de chien
+poids = {"Yorkshire Terrier": 0.18, "Shih Tzu": 0.17, "Chihuahua": 0.14, "Maltese": 0.11, "Labrador Retriever": 0.09}
+
+# Calculer le total pondéré pour chaque zone ZIP
+def calcul_total(row):
+    total = sum([row[breed] * poids[breed] for breed in poids])
+    return total
+
+tableau_croise["Total pondéré"] = tableau_croise.apply(calcul_total, axis=1)
+
+# Trier le tableau croisé par ordre décroissant sur la colonne "Total pondéré"
+tableau_croise = tableau_croise.sort_values(by="Total pondéré", ascending=False)
+
+# Afficher le résultat de la colonne "Total pondéré" pour les 10 zones ZIP les plus occupées par ces chiens
+resultats = tableau_croise["Total pondéré"].head(10)
+print(resultats)
+
+# Assigner les valeurs Zip code et score correspondant à des variables
+zipcodes = resultats.index.tolist()
+scores = resultats.values.tolist()
+
+# Utiliser ces variables dans un autre code
+autre_variable = zipcodes[0] + scores[0]
---
+++ Zip code.py
@@ -0,0 +1,52 @@
+import folium
+import json
+from geopy.geocoders import Nominatim
+from branca.colormap import linear
+
+# charger les données GeoJSON
+with open(r"C:\Users\33782\Desktop\M2 MSI\S2\Certification\carte_OSM\NYC.geojson") as f:
+    data = json.load(f)
+
+# créer une carte centrée sur les données
+center = [40.70879507039175,-74.00098833771662]
+map = folium.Map(location=center, zoom_start=13)
+
+# Définir le rayon maximal et le score maximal
+max_radius =  500
+max_score = 10
+
+# Créer un objet géocodeur Nominatim
+geolocator = Nominatim(user_agent="my-app")
+
+# parcourir les features pour ajouter chaque feature à la carte selon sa géométrie
+for feature in data["features"]:
+    name = feature["properties"]["name"]
+    if feature["geometry"]["type"] == "MultiPolygon":
+        # si la géométrie est un multipolygone, créer un objet GeoJson avec les propriétés de la feature
+        folium.GeoJson(feature, 
+                       name=name, 
+                       style_function=lambda x: {'color': 'red', 'weight': 5}).add_to(map) # Ajout de style à la géométrie
+
+# Liste des 10 codes ZIP et scores correspondants
+zip_codes = ["10025","10009","10023","11201","10011","11215","10024","10128","10029","10003"]
+scores = [8, 6, 9, 7, 5, 6, 8, 9, 7, 6]
+
+# Ajouter un marqueur et un cercle pour chaque code ZIP avec un score personnalisé
+for i, code in enumerate(zip_codes):
+    # Obtenir les coordonnées géographiques du code ZIP à l'aide du géocodeur
+    location = geolocator.geocode(code + ", USA")
+    if location is not None:
+        # Ajouter un marqueur pour les coordonnées géographiques obtenues avec l'étiquette personnalisée
+        folium.Marker(location=[location.latitude, location.longitude], 
+                      tooltip=f"{code}, Score: {scores[i]}").add_to(map)
+
+        # Ajouter un cercle avec une couleur et une taille en fonction du score
+        radius = 2 * max_radius * scores[i] / max_score
+        folium.Circle(location=[location.latitude, location.longitude], 
+                      radius=radius, 
+                      color='orange', 
+                      fill=True, 
+                      fill_opacity=0.8).add_to(map)
+
+# Afficher la carte
+map.save(r"C:\Users\33782\Desktop\M2 MSI\S2\Certification\carte_OSM\map.html")
